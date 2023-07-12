6ac4a727195a36a8bf39545b20c7a042b99e64e6
Andrew Chon <andrewyhchon@gmail.com>
2023-07-11 23:36:40 -0700

    did hero Class didnt implement hero having more attacks than monster

--- DungeonAdventure/src/Model/Heroes.java
+++ DungeonAdventure/src/Model/Heroes.java
@@ -2,8 +2,21 @@ package Model;
 
 public abstract class Heroes extends DungeonCharacter {
 
-    // still need to change not done
-    protected Heroes(int theHp, String theChName, int theRange, int theAttackSpeed, int theHitChance) {
-        super(theHp, theChName, theRange, theAttackSpeed, theHitChance);
+    private double myBlockChance;
+
+    protected Heroes(int theHp, String theChName, int theAttackSpeed, int theMinDamage, int theMaxDamage, double theHitChance, double theBlockChance) {
+        super(theHp, theChName, theAttackSpeed, theMinDamage, theMaxDamage, theHitChance);
+        myBlockChance = theBlockChance;
     }
+
+    public double getBlockChance() {
+        return myBlockChance;
+    }
+    public void setBlockChance(double theBlockChance) {
+        myBlockChance = theBlockChance;
+    }
+    public abstract void regularAttack(DungeonCharacter opponent);
+
+    public abstract void specialSkill(DungeonCharacter opponent);
+
 }
--- DungeonAdventure/src/Model/Warrior.java
+++ DungeonAdventure/src/Model/Warrior.java
@@ -1,9 +1,19 @@
 package Model;
 
 public class Warrior extends Heroes{
-
-    // this constructor still needs changes to hardcode in stats
-    protected Warrior(int theHp, String theChName, int theRange, int theAttackSpeed, int theHitChance) {
-        super(theHp, theChName, theRange, theAttackSpeed, theHitChance);
+    protected Warrior(int theHp, String theChName, int theAttackSpeed, int theMinDamage, int theMaxDamage, double theHitChance, double theBlockChance) {
+        super(theHp, theChName, theAttackSpeed, theMinDamage, theMaxDamage, theHitChance, theBlockChance);
     }
+
+    @Override
+    public void regularAttack(DungeonCharacter opponent) {
+
+    }
+
+    @Override
+    public void specialSkill(DungeonCharacter opponent) {
+
+    }
+
+
 }
