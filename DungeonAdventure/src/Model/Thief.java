f9e8ecb1972c4592ce516e10e38b5463bb3bb891
Andrew Chon <andrewyhchon@gmail.com>
2023-07-12 18:34:33 -0700

    Thief constructor update

--- DungeonAdventure/src/Model/Thief.java
+++ DungeonAdventure/src/Model/Thief.java
@@ -4,8 +4,8 @@ public class Thief extends Heroes{
     private static final double SURPRISE_ATTACK_CHANCE = 0.4;
     private static final double SURPRISE_FAIL_CHANCE = 0.2;
 
-    protected Thief(int theHp, String theChName, int theAttackSpeed, int theMinDamage, int theMaxDamage, double theHitChance, double theBlockChance) {
-        super(theHp, theChName, theAttackSpeed, theMinDamage, theMaxDamage, theHitChance, theBlockChance);
+    protected Thief() {
+        super(75, "Thief", 6, 20, 40, 0.8, 0.4);
     }
 
     @Override
