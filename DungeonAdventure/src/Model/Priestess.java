ba4bfa1c2ea50e840b60180d02dda94cbea43679
Andrew Chon <andrewyhchon@mgmail.com>
2023-07-14 17:54:53 -0700

    added javadoc for priestess and fixed magic numbers

--- DungeonAdventure/src/Model/Priestess.java
+++ DungeonAdventure/src/Model/Priestess.java
@@ -5,20 +5,67 @@ import Control.Keyboard;
 
 import java.awt.*;
 
+/**
+ * Priestess class which has the ability to heal and
+ * extends from the Heroes class.
+ */
 public class Priestess extends Heroes{
 
+    /**
+     * Min heal that priestess can heal for.
+     */
     private static final int MIN_HEAL = 25;
+
+    /**
+     * the max amount priestess can heal for.
+     */
     private static final int MAX_HEAL = 55;
 
-    private int myHp;
-    private String myChName;
-    private int myAttackSpeed;
-    private int myMinDamage;
-    private int myMaxDamage;
-    private double myHitChance;
+    /**
+     * Hp of priestess 75.
+     */
+    private static final int HP = 75;
 
+    /**
+     * Setting Priestess name.
+     */
+    private static final String NAME = "Priestess";
+
+    /**
+     * Setting this characters attack speed to 5.
+     */
+    private static final int ATTACK_SPEED = 5;
+
+    /**
+     * Minimum damage of priestess.
+     */
+    private static final int MIN_DAMAGE = 25;
+
+    /**
+     * Maximum damage of priestess.
+     */
+    private static final int MAX_DAMAGE = 45;
+
+    /**
+     * Hit chance of priestess.
+     */
+    private static final double HIT_CHANCE = 0.7;
+
+    /**
+     * Block chance of priestess.
+     */
+    private static final double BLOCK_CHANCE = 0.3;
+
+    /**
+     * Priestess constructor which sets up all her stats
+     * in hp, name, attack speed, damage, hit chance and block chance.
+     * Also sets up background for the characters for the view.
+     *
+     * @param theGamePanel gamepanel of character
+     * @param theKeyboard keyboard input of character
+     */
     public Priestess(GamePanel theGamePanel, Keyboard theKeyboard) {
-        super(75, "Priestess", 5, 25, 45, 0.7, 0.3, theGamePanel,theKeyboard);
+        super(HP, NAME, ATTACK_SPEED, MIN_DAMAGE, MAX_DAMAGE, HIT_CHANCE, BLOCK_CHANCE, theGamePanel,theKeyboard);
     }
     @Override
     public void getHeroesImage() {
@@ -28,15 +75,34 @@ public class Priestess extends Heroes{
     public double getBlockChance() {
         return super.getBlockChance();
     }
+
+    /**
+     * Priestess normal attack which calculates her min and max
+     * damage to get something in between.
+     *
+     * @param theOpp opponent in which the attack will be targeted towards.
+     */
     @Override
     public void regularAttack(DungeonCharacter theOpp) {
         attackBehavior(theOpp);
     }
+
+    /**
+     * Priestess special which is a healing ability to herself or allies.
+     *
+     * @param theAlly which the special skill will be aimed towards.
+     */
     @Override
     public void specialSkill(DungeonCharacter theAlly) {
         theAlly.setHp(getHp() + genHeal());
     }
 
+    /**
+     * Helper method to generate heal of the priestess
+     * from her min and max heal using Math.random().
+     *
+     * @return range of heal from min to max
+     */
     private int genHeal() {
         return (int) (Math.random() * ((MIN_HEAL - MAX_HEAL + 1))) + MIN_HEAL;
     }
