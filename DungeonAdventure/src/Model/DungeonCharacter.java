9cfe889315c13acfb60b9c57d4d8605da4ac19d7
Andrew Chon <andrewyhchon@mgmail.com>
2023-07-25 16:06:12 -0700

    fixed priestes heal might need to nerf

--- DungeonAdventure/src/Model/DungeonCharacter.java
+++ DungeonAdventure/src/Model/DungeonCharacter.java
@@ -160,7 +160,8 @@ public abstract class DungeonCharacter {
      * @return the value that damage will be done between min and max.
      */
     public int genDamage(int theMin, int theMax) {
-        return (int) (Math.random() * ((theMin - theMax + 1))) + getMinDamage();
+        return (int) Math.floor(Math.random() * (theMax-theMin) + theMin);
+//        return (int) (Math.random() * (theMin - theMax)+ getMinDamage());
     }
 
     public boolean isAlive() {
--- DungeonAdventure/src/View/Main.java
+++ DungeonAdventure/src/View/Main.java
@@ -6,9 +6,22 @@ import Model.*;
 public class Main {
 
     public static void main(String[] args){
-        MainFrame frame = new MainFrame();
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
+//        MainFrame frame = new MainFrame();
+//        frame.setVisible(true);
+//        frame.setResizable(false);
+//        frame.setLocationRelativeTo(null);
+
+        GamePanel gp = new GamePanel();
+        Keyboard kb = new Keyboard(gp);
+        Heroes myHero = new Priestess(gp, kb);
+        Monster myMonster = new Skeleton();
+
+        Battle newBattle = new Battle(myHero, myMonster);
+        String[] array = newBattle.getMyBattleLog();
+
+        for(String s: array) {
+            System.out.println(s);
+        }
+
     }
 }
