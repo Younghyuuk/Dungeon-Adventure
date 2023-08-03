4013c1c60cf66e6c3985792df2cd81ef6131205e
Andrew Chon <andrewyhchon@gmail.com>
2023-08-02 18:59:48 -0700

    making sure main is consistent

--- DungeonAdventure/src/View/Main.java
+++ DungeonAdventure/src/View/Main.java
@@ -8,23 +8,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-//            MainFrame frame = new MainFrame();
-//            frame.setVisible(true);
-//            frame.setResizable(false);
-//            frame.setLocationRelativeTo(null);
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+            frame.setResizable(false);
+            frame.setLocationRelativeTo(null);
 
 
-        GamePanel gp = new GamePanel();
-        Keyboard kb = new Keyboard(gp);
-        Heroes myHero = new Thief(gp, kb);
-        Monster myMonster = new MonsterDataBase(gp).getRandomMonster();
-
-        Battle newBattle = new Battle(myHero, myMonster);
-        String[] array = newBattle.getMyBattleLog();
-
-        for (String s : array) {
-            System.out.println(s);
-        }
+//        GamePanel gp = new GamePanel();
+//        Keyboard kb = new Keyboard(gp);
+//        Heroes myHero = new Thief(gp, kb);
+//        Monster myMonster = new MonsterDataBase(gp).getRandomMonster();
+//
+//        Battle newBattle = new Battle(myHero, myMonster);
+//        String[] array = newBattle.getMyBattleLog();
+//
+//        for (String s : array) {
+//            System.out.println(s);
+//        }
 
 
     }
