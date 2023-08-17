fddd638e86bd01829ca906fbd3d95310b513301e
Marrok Young <marrokyoung@Marroks-MacBook-Air.local>
2023-08-16 20:15:18 -0700

    Added JUnit tests for HealthPotion and FourPillars

---
+++ DungeonAdventure/src/Model/HealthPotionTest.java
@@ -0,0 +1,30 @@
+package Model;
+
+import View.GamePanel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.image.BufferedImage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HealthPotionTest {
+    private HealthPotion myHealthPot;
+    @BeforeEach
+    public final void setUp() {
+        GamePanel gp = new GamePanel();
+        myHealthPot = new HealthPotion(100, 100, gp);
+    }
+
+    @Test
+    void getHealthBack() {
+        int healthBack = myHealthPot.getHealthBack();
+        assertTrue(healthBack >= myHealthPot.getMinHealth() && healthBack <= myHealthPot.getMaxHealth());
+    }
+
+    @Test
+    void getItemImage() {
+        BufferedImage itemImage = myHealthPot.getHealthPotionImage();
+        assertNotNull(itemImage);
+    }
+}
